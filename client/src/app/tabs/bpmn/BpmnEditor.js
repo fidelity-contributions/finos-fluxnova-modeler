bffd096b6520ad78720fb4706e21072bd6691706
a737206_fidelity <suman.kumarpani@fmr.com>
2026-08-04 14:57:51 +0530

    fix eslint observation

--- client/src/app/tabs/bpmn/BpmnEditor.js
+++ client/src/app/tabs/bpmn/BpmnEditor.js
@@ -475,7 +475,7 @@ export class BpmnEditor extends CachedComponent {
       modeler
     } = this.getCached();
 
-    const engineProfile = engineProfileOverride || cachedEngineProfile;    
+    const engineProfile = engineProfileOverride || cachedEngineProfile;
 
     if (!engineProfile) {
       return;
